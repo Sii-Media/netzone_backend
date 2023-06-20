57e60aaf6237b7ef1e8e8722b2d2435cc6b3dd99
WissamKahwaji <kahwajiwisam@gmail.com>
2023-06-20 15:52:33 +0400

    add tenders

--- controllers/departmenst/departmentsCtrl.js
+++ controllers/departmenst/departmentsCtrl.js
@@ -48,7 +48,13 @@ export const getProductsByCategory = async (req, res) => {
             return res.status(404).json({ message: `Department ${department} not found` });
         }
         console.log(departments._id);
-        const categories = await DepartmentsCategory.findOne({ name: category, department: departments._id }).populate('products');
+        const categories = await DepartmentsCategory.findOne({ name: category, department: departments._id }).populate('products').populate({
+            path: 'products',
+            populate: {
+                path: 'category',
+                select: 'name',
+            },
+        });
         console.log(categories);
         if (!categories) {
             return res.status(404).json({ message: `Category ${category} not found in department ${department}` });
