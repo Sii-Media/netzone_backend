f3e170b2809c6833b8dd7cbb06a6e2a6313952cf
WissamKahwaji <kahwajiwisam@gmail.com>
2023-08-02 13:14:40 +0400

    edit getuserproducts

--- controllers/departmenst/departmentsCtrl.js
+++ controllers/departmenst/departmentsCtrl.js
@@ -32,7 +32,7 @@ export const getAllProducts = async (req, res) => {
 export const getProductById = async (req, res) => {
     const { productId } = req.params;
     try {
-        const product = await Product.findById(productId).populate('category', 'name').populate('owner', 'username');
+        const product = await Product.findById(productId).populate('category', 'name').populate('owner', 'username userType');
         return res.json(product);
     } catch (error) {
         return res.status(500).json({ message: error.message });
