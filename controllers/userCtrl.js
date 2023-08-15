0afa1352534e454978f63960f43bd0f47df14c84
WissamKahwaji <kahwajiwisam@gmail.com>
2023-08-15 10:27:44 +0400

    edits

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -77,9 +77,9 @@ export const signUp = async (req, res) => {
         const { country } = req.body;
         const { username, email, password, userType, firstMobile, secondeMobile, thirdMobile, isFreeZoon, isService, freezoneCity, deliverable, subcategory, address, website, link, slogn, businessLicense, companyProductsNumber, sellType, toCountry, isThereWarehouse, isThereFoodsDelivery, deliveryType, deliveryCarsNum, deliveryMotorsNum } = req.body;
         const { title } = req.body;
-        const profilePhoto = req.files['profilePhoto'][0] ? req.files['profilePhoto'][0] : null;
+        const profilePhoto = req.files['profilePhoto'] ? req.files['profilePhoto'][0] : null;
         const bannerPhoto = req.files['bannerPhoto'] ? req.files['bannerPhoto'][0] : null;
-        const coverPhoto = req.files['coverPhoto'][0] ? req.files['coverPhoto'][0] : null;
+        const coverPhoto = req.files['coverPhoto'] ? req.files['coverPhoto'][0] : null;
         const frontIdPhoto = req.files['frontIdPhoto'] ? req.files['frontIdPhoto'][0] : null;
         const backIdPhoto = req.files['backIdPhoto'] ? req.files['backIdPhoto'][0] : null;
 
