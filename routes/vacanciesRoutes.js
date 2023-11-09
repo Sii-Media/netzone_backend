1047bd83c6d8f9f85bce07fa698981339bde2b17
siimediasub <siimediagithub@gmail.com>
2023-11-09 12:41:59 +0400

    deletes

--- routes/vacanciesRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getVacancies } from '../controllers/vacanciesCtrl.js';
-import auth from '../middlewares/auth.js';
-
-const router = express.Router();
-
-router.get('/', auth, getVacancies);
-
-export default router;
